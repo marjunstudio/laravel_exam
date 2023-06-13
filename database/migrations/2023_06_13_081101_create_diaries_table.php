e4389c688d2785047fedb108d03f4c32fa8d4b67
marjunstudio <uebujie@gmail.com>
2023-06-13 23:09:57 +0900

    Fix:マイグレーションファイルを複数形に修正

--- database/migrations/2023_06_13_081101_create_diaries_table.php
+++ database/migrations/2023_06_13_081101_create_diaries_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('diary', function (Blueprint $table) {
+        Schema::create('diaries', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->text('content');
