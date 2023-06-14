a50df99d86de622b91e54cddcdae47ba155d2af2
marjunstudio <uebujie@gmail.com>
2023-06-14 16:05:46 +0900

    Fix:DiaryIndexビューにヘッダーを表示

--- resources/views/diary/index.blade.php
+++ resources/views/diary/index.blade.php
@@ -1,20 +1,14 @@
-<html>
-  <head>
-  <title>Hello/Index</title>
-    <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-    </style>
-    @vite('resources/css/app.css')
-  </head>
-  <body>
-    <h1>DiaryIndex</h1>
-    <h3>投稿一覧</h3>
-    @foreach ($items as $item)
-    <p>タイトル：{{ $item->title }}</p>
-    <p>内容：{{ $item->content }}</p>
-    <p>{{ $item->created_at }}</p>
+@extends('layouts.app')
 
-    @endforeach
-  </body>
-</html>
+@section('title', 'DiariesCreate')
+
+@section('content')
+  <h1>DiaryIndex</h1>
+  <h3>投稿一覧</h3>
+  @foreach ($items as $item)
+  <p>タイトル：{{ $item->title }}</p>
+  <p>内容：{{ $item->content }}</p>
+  <p>{{ $item->created_at }}</p>
+
+  @endforeach
+@endsection
