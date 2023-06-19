3aa8cecab262f8606e3150259b33c5a101147351
marjunstudio <uebujie@gmail.com>
2023-06-19 23:54:29 +0900

    Add:adminと作成者以外のアクションを制限

---
+++ app/Http/Middleware/CheckAdminAuthorization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdminAuthorization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::user()->is_admin === false && Auth::id() !== $request->user_id) {
+            return redirect()->back()->with('msg', '権限がありません。');
+        }
+
+        return $next($request);
+    }
+}
