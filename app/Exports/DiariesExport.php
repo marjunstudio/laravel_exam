5ea1e874785d4543df5896119f22143e4455c6cf
marjunstudio <uebujie@gmail.com>
2023-06-19 14:26:09 +0900

    Add:Diaryエクスポート時にヘッダーを追加

--- app/Exports/DiariesExport.php
+++ app/Exports/DiariesExport.php
@@ -6,10 +6,22 @@ use App\Models\Diary;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class DiariesExport implements FromCollection
+class DiariesExport implements FromCollection, WithHeadings
 {
 	public function collection()
 	{
 		return Diary::all();
 	}
+
+	public function headings(): array
+{
+    return [
+        'id',
+        'タイトル',
+        'コンテンツ',
+        '作成日時',
+        '更新日時',
+				'ユーザーID',
+    ];
+}
 }
