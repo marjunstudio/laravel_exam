b9f733a299f1b13d178140afd6a77906eb7dc436
marjunstudio <uebujie@gmail.com>
2023-06-30 16:34:22 +0900

    Fix:プロフィール画面のレイアウトを調整

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,48 +8,37 @@
     <div class="mb-10 md:mb-3">
       <h2 class="text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">ユーザープロフィール</h2>
     </div>
-
     {{-- ユーザー情報 --}}
     <div class="lg:w-1/2 md:w-2/3 mx-auto">
-      <div class="flex flex-wrap -m-2">
-        <div class="flex mx-auto border-slate-300">
+      <div class="flex flex-wrap">
+        <div class="flex mx-auto">
           {{-- プロフィールアイコン --}}
-          <div class="p-2">
-            <div class="relative">
-              <p class="w-full rounded focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                <img src="{{ asset($user->image) }}" class="w-14 h-14">
-              </p>
-            </div>
+          <div class="justify-center mr-2">
+            <img src="{{ asset($user->image) }}" class="w-20 h-20 rounded-full inline-flex items-center justify-center">
           </div>
           {{-- ユーザー名 --}}
-          <div class="p-2 mr-10">
-            <div class="relative">
-              <label for="name" class="leading-7 text-sm text-gray-600">ユーザー名</label>
-              <p class="w-full bg-opacity-50 rounded focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                {{ $user->name }}
-              </p>
-            </div>
+          <div class="mr-20 flex item-center">
+            <p class="flex justify-center items-center w-full text-lg outline-none font-bold text-gray-700 leading-8 transition-colors duration-200 ease-in-out">
+              {{ $user->name }}
+            </p>
           </div>
-          {{-- メールアドレス --}}
-          <div class="p-2">
-            <div class="relative">
-              <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
-              <p class="w-full rounded focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                {{ $user->email }}
-              </p>
-            </div>
+          {{-- プロフィール編集ボタン --}}
+          <div class="flex justify-center items-center">
+            <a href="{{ route('user.edit', ['id'=>$user->id]) }}" class="rounded-lg bg-indigo-500 px-5 py-2 text-center text-sm font-medium text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 active:bg-indigo-700 md:text-base">
+              <button>プロフィールを編集</button>
+            </a>
           </div>
         </div>
-        {{-- プロフィール編集ボタン --}}
-        <a href="{{ route('user.edit', ['id'=>$user->id]) }}">編集</a>
-
-        <div class="flex flex-col text-center w-full mt-12">
-          <h1 class="sm:text-2xl text-xl font-medium title-font mb-4 text-gray-900">投稿した日記</h1>
-        </div>
-        
-        {{-- 投稿一覧表示 --}}
-        <x-diary_index :items="$diaries" />
       </div>
     </div>
-  </section>
+  </div>
+</div>
+<div class="flex flex-col text-center w-full mt-12">
+  <h2 class="text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">投稿した日記</h2>
+</div>
+
+{{-- 投稿一覧表示 --}}
+<x-diary_index :items="$diaries" />
+
+
 @endsection
