14daf706618b29bb02786e5978de397946207702
marjunstudio <uebujie@gmail.com>
2023-07-05 10:02:14 +0900

    Fix:Admin権限のアクションを整理

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,18 @@ Route::get('/', function () {
     return view('home.static');
 })->middleware('guest');;
 
+// 管理者のみアクセス可能
+Route::middleware('admin')->group(function() {
+    // ダッシュボード
+    Route::get('dashboard/user', [DashboardController::class, 'user_index'])->name('dashboard.user_index');
+    Route::get('dashboard/diary', [DashboardController::class, 'diary_index'])->name('dashboard.diary_index');
+    // CSVのインポート・エクスポート
+    Route::get('/diary/csv-export', [DiaryController::class, 'csvExport'])->name('diary_csv.export');
+    Route::post('/diary/csv-import', [DiaryController::class, 'csvImport'])->name('diary_csv.import');
+    Route::get('/user/csv-export', [UserController::class, 'csvExport'])->name('user_csv.export');
+    Route::post('/user/csv-import', [UserController::class, 'csvImport'])->name('user_csv.import');
+});
+
 // Diary関係
 Route::controller(DiaryController::class)->middleware('auth')->group(function() {
     Route::get('diary/create', 'create')->name('diary.create');
@@ -38,16 +50,4 @@ Route::controller(UserController::class)->middleware('auth')->group(function() {
     Route::get('profile', 'profile')->name('user.profile');
     Route::get('profile/{id}/edit', 'edit')->name('user.edit');
     Route::put('profile/{id}', 'update')->name('user.update');
-});
-
-// 管理者のみアクセス可能
-Route::middleware('admin')->group(function() {
-    // ダッシュボード
-    Route::get('dashboard/user', [DashboardController::class, 'user_index'])->name('dashboard.user_index');
-    Route::get('dashboard/diary', [DashboardController::class, 'diary_index'])->name('dashboard.diary_index');
-    // CSVのインポート・エクスポート
-    Route::get('/diary/csv-export', [DiaryController::class, 'csvExport'])->name('diary_csv.export');
-    Route::post('/diary/csv-import', [DiaryController::class, 'csvImport'])->name('diary_csv.import');
-    Route::get('/user/csv-export', [UserController::class, 'csvExport'])->name('user_csv.export');
-    Route::post('/user/csv-import', [UserController::class, 'csvImport'])->name('user_csv.import');
 });
