76239772e510e0c1ccfa8e845f027483984b76b4
marjunstudio <uebujie@gmail.com>
2023-07-05 09:51:21 +0900

    Add:ログイン必須のアクションにフラッシュメッセージを追加

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class Authenticate extends Middleware
 {
@@ -12,6 +13,11 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
+        $message = 'ログインが必要です。'; // 表示するメッセージを設定する
+
+        // メッセージとスタイルを連想配列として保存する
+        Session::flash('msg', $message);
+
         return $request->expectsJson() ? null : route('login');
     }
 }
